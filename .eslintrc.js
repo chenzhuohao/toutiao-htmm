5a5e7d2e888c4bc7a8f6b0ff91aed9d6b125336a
chenzhuohao <1162797366@qq.com>
2022-08-30 11:27:07 +0800

    '初始化'

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true
+    },
+    extends: [
+        'plugin:vue/essential',
+        '@vue/standard'
+    ],
+    parserOptions: {
+        parser: '@babel/eslint-parser'
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        // eslint-disable-next-line quote-props
+        'indent': 0,
+        'eol-last': 0
+    }
+}
